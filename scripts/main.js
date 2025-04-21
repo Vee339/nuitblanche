dceb6a51deff13eb7fa0244694ef20697706cc2c
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-04-21 08:55:06 -0400

    Build the layout of homepage

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  //   const header = document.querySelector("header");
+  //   const main = document.querySelector("main");
+  //   const headerCompStyles = window.getComputedStyle(header);
+  //   header.style.paddingTop = headerCompStyles.getPropertyValue("height") + "px";
+};
